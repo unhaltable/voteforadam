5a85eb98e70c5a9aefeb53815a1c819edf5dcacc
arkon <ey.cheung@outlook.com>
2015-03-17 14:38:13 -0400

    Minify JS/CSS.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-$(document).ready(function(){
-  var stickyNavTop, scrollTop;
+$(document).ready(function() {
+  var stickyNav = function() {
+    var stickyNavTop = window.innerHeight,
+        scrollTop    = $(window).scrollTop();
 
-  var stickyNav = function(){
-    stickyNavTop = window.innerHeight;
-    scrollTop = $(window).scrollTop();
-
-    $("header").css("opacity", 1 - scrollTop / stickyNavTop);
+    $('header').css('opacity', 1 - scrollTop / stickyNavTop);
 
     if (scrollTop > stickyNavTop) {
       $('#nav').addClass('sticky');
@@ -16,26 +14,26 @@ $(document).ready(function(){
     }
   };
 
-  stickyNav();
-  $(window).scroll(function(){ stickyNav(); });
-  window.onresize = function(){ stickyNav(); }
-
-  // Smooth scrollTo
   var scrollTo = function(el, off){
     $('html, body').animate({
       scrollTop: $(el).offset().top - off
     }, 650);
-  }
+  };
 
-  $("header").click(function(){ scrollTo("nav", 0); });
-  $("#more").click(function(){ scrollTo("nav", 0); });
-  $("#title").click(function(){ scrollTo("body", 0); });
-  $("#navabout").click(function(){
-    if (scrollTop <= stickyNavTop) scrollTo("nav", 0);
-    else scrollTo("#about", 0);
+  stickyNav();
+
+  $(window).scroll(stickyNav);
+  window.onresize = stickyNav;
+
+  $('header').click(function() { scrollTo('nav', 0); });
+  $('#more').click(function() { scrollTo('nav', 0); });
+  $('#title').click(function() { scrollTo('body', 0); });
+  $('#navabout').click(function() {
+    if (scrollTop <= stickyNavTop) scrollTo('nav', 0);
+    else scrollTo('#about', 0);
   });
-  $("#navgoals").click(function(){ scrollTo("#goals", 50); });
-  $("#navvote").click(function(){ scrollTo("#vote", 50); });
-  $("#navcontact").click(function(){ scrollTo("#contact", 50); });
-  $("#navright").click(function(){ scrollTo("#vote", 50); });
+  $('#navgoals').click(function() { scrollTo('#goals', 50); });
+  $('#navvote').click(function() { scrollTo('#vote', 50); });
+  $('#navcontact').click(function() { scrollTo('#contact', 50); });
+  $('#navright').click(function() { scrollTo('#vote', 50); });
 });
